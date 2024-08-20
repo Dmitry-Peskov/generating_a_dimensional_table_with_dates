81bd55d1924bac2dc0ce704873b5b323742e8bdb
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-08-20 22:35:37 +0300

    В блоке if __name__ == "__main__" объявил весь алгоритм работы скрипта. Так же объявил все функции (без реализации) с помощью которых будет достигаться поставленная цель

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from datetime import datetime
 
 
 FIELDS_LIST: list[str] = [
@@ -17,5 +18,35 @@ FIELDS_LIST: list[str] = [
 FIELDS_STR = ", ".join(FIELDS_LIST) # "datetime, date, year, month, month_name_ru, month_name_en, day, week_num, week_day, quarter, is_weekends"
 DateMetaData = namedtuple(typename="DateMetaData", field_names=FIELDS_STR)
 
+def get_next_day(date: datetime) -> datetime:
+    raise NotImplemented
+
+
+def extract_metadata_from_date(date: datetime) -> DateMetaData:
+    raise NotImplemented
+
+
+def add_data_to_storage(date_metadata: DateMetaData, storage: list[dict]) -> None:
+    raise NotImplemented
+
+
+def generate_filename(start: datetime, end: datetime) -> str:
+    raise NotImplemented
+
+
+def write_to_csv(filename: str, data: list[dict]) -> None:
+    raise NotImplemented
+
+
 if __name__ == "__main__":
-    pass
+    start_date: datetime = datetime(year=2000, month=1, day=1)  # начало диапазона дат
+    end_date: datetime = datetime(year=2100, month=1, day=1)    # окончание диапазона дат
+    result_list: list[dict] = []                                # список для промежуточного хранения результирующей последовательности дат
+    current_processed_date: datetime = start_date               # текущая дата из которой извлекаются метаданные и сохраняются в промежуточный результат
+    # в цикле перебираем каждую дату из заданного диапазона, сохраняем метаданные каждой в result_list
+    while current_processed_date < end_date:
+        metadata: DateMetaData = extract_metadata_from_date(date=current_processed_date)
+        add_data_to_storage(date_metadata=metadata, storage=result_list)
+        current_processed_date:datetime = get_next_day(date=current_processed_date)
+    file_name: str = generate_filename(start=start_date, end=end_date)
+    write_to_csv(filename=file_name, data=result_list)
