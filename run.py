86a99d064662299b9e693bc22c3d08f0b4830270
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-08-20 23:22:15 +0300

    Реализовал логику для функции write_to_csv, которая производит запись полученного результата в файл с расширением .csv

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import csv
 from collections import namedtuple
 from datetime import datetime, timedelta
 from typing import Optional
@@ -65,13 +66,16 @@ def generate_filename(start: datetime, end: datetime) -> str:
     return f"dimensional_table_with_dates_{start.strftime('%d.%m.%Y')}-{end.strftime('%d.%m.%Y')}.csv"
 
 
-def write_to_csv(filename: str, data: list[dict]) -> None:
-    raise NotImplemented
+def write_to_csv(filename: str, fields_name: list[str], data: list[dict]) -> None:
+    with open(filename, "w", encoding="utf-8", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fields_name)
+        writer.writeheader()
+        writer.writerows(data)
 
 
 if __name__ == "__main__":
     start_date: datetime = datetime(year=2000, month=1, day=1)  # начало диапазона дат
-    end_date: datetime = datetime(year=2100, month=1, day=1)    # окончание диапазона дат
+    end_date: datetime = datetime(year=2050, month=1, day=1)    # окончание диапазона дат
     result_list: list[dict] = []                                # список для промежуточного хранения результирующей последовательности дат
     current_processed_date: datetime = start_date               # текущая дата из которой извлекаются метаданные и сохраняются в промежуточный результат
     # в цикле перебираем каждую дату из заданного диапазона, сохраняем метаданные каждой в result_list, до тех пор пока не превысим end_date
@@ -80,4 +84,4 @@ if __name__ == "__main__":
         add_data_to_storage(date_metadata=metadata, storage=result_list)                  # сохраняем метаданные в result_list
         current_processed_date:datetime = get_next_day(date=current_processed_date)       # переключаемся на следующую дату
     file_name: str = generate_filename(start=start_date, end=end_date)                    # генерируем название файла по шаблону
-    write_to_csv(filename=file_name, data=result_list)  # записываем результат в csv файл
+    write_to_csv(filename=file_name, fields_name=FIELDS_LIST, data=result_list)  # записываем результат в csv файл
