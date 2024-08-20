b113d243625f4f149ea84bb4d08f61b300829758
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-08-20 21:38:42 +0300

    Добавил константы FIELDS_LIST и FIELDS_STR, которые содержат в себе названия будущих свойств (столбцов) для отдельно взятой даты. Они будут использоваться при объявлении NamedTuple и сохранении результатов работы в файл .csv

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
-
+FIELDS_LIST: list[str] = [
+    "datetime",       # 2024-08-20 00:00:00
+    "date",           # 2024-08-20
+    "year",           # 2024
+    "month",          # 8
+    "month_name_ru",  # август
+    "month_name_en",  # august
+    "day",            # 20
+    "week_num",       # 34
+    "week_day",       # 2
+    "quarter",        # 3
+    "is_weekends"     # 0
+]
+FIELDS_STR = ", ".join(FIELDS_LIST) # "datetime, date, year, month, month_name_ru, month_name_en, day, week_num, week_day, quarter, is_weekends"
 
 if __name__ == "__main__":
     pass
